3c25b9c0fd54c7bbd3dbe918dd4403dfe3633412
trevor-mill <trevor.million@chicagobooth.edu>
2022-02-08 17:14:00 +0000

    playing with director view

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,6 +1,8 @@
 class DirectorsController < ApplicationController
 
   def index
+    @list_of_directors = Director.all
+    
 
     render({ :template => "director_templates/index.html.erb"})
   end
