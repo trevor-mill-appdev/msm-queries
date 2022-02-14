817958a142ee819c177fb53b5ec7ae6a76981242
trevor-mill <trevor.million@chicagobooth.edu>
2022-02-14 13:47:02 +0000

    youngest working except for link

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,4 +6,13 @@ class DirectorsController < ApplicationController
 
     render({ :template => "director_templates/index.html.erb"})
   end
+
+  def youngest
+    @min_dob = Director.minimum(:dob)
+    @youngest = Director.where({ :dob => @min_dob }).first
+    
+
+    render({ :template => "director_templates/youngest_director.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors", { :controller => "directors", :action => "index" })
+
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+  
+  get("/directors/oldest", { :controller => "directors", :action => "oldest" })
+  
 end
